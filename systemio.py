b14a81b673e45912bf91204c0a4130a47757c8fb
jasonmpittman <jason@jasonmpittman.com>
2021-08-27 11:37:56 -0400

    updated page analysis logic

--- systemio.py
+++ systemio.py
@@ -33,19 +33,36 @@ def get_page_count(pdf):
     return len(pages)
 
 def list_metadata_keywords(pdf):
-    return pdf.metadata['Keywords']
+    return pdf.metadata['Keywords'].split(';')
 
 def list_paper_keywords():
     pass
 
-def analyze_paper(file, page_count):
+def analyze_paper(file, page_count, keywords):
     fd = open(file, "rb")
     pdf = SimplePDFViewer(fd)
+
+    pdf_pages = {}
+    keyword_frequency = {}
 
     for i in range(1, page_count):
         pdf.navigate(i)
         pdf.render()
-        print(pdf.canvas.strings)
+
+        pdf_pages[i] = pdf.canvas.strings
+
+    for keyword in keywords:
+        keyword_frequency[keyword] = count_keyword(keyword, pdf_pages)    
+
+    print(keyword_frequency)
+
+def count_keyword(keyword, pages):
+
+    for page_number, page_content in pages.items():
+        print("Searching for {} on page {}...".format(keyword, page_number))
+        count = page_content.count(keyword) # this isn't finding the keywords...?
+
+    return count
 
 def main():
     parser = argparse.ArgumentParser(description='Analyze a PDF for keyword structural bibliometrics')
@@ -57,12 +74,12 @@ def main():
         pdf = load_pdf(args.file[0])
         
         keywords = list_metadata_keywords(pdf)
-        print(keywords)
+        print(keywords) #for sun2019 this has an extra empty element at the end of the list
 
         page_count = get_page_count(pdf)
         print(page_count)
         
-        analyze_paper(args.file[0], page_count)
+        analyze_paper(args.file[0], page_count, keywords)
     else:
         print(parser.print_help())
 
